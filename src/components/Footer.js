52e0395bf5244fe404a7b489e24304549a92ce68
Parthivi-Jain-24 <161028902+Parthivi-Jain-24@users.noreply.github.com>
2024-12-20 12:51:09 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ background: '#028900', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>&copy; 2024 Agriplast. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
